daee88a6109ca4a3f4e16f4cc816fac0302e0c24
SYXZ <49202777+SYXZyt@users.noreply.github.com>
2023-06-03 20:39:12 +0100

    Added main function

---
+++ PythonPlus/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <sstream>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	//Check how many arguments we were provided
+	//If we only got one, we want to print an error
+	//Remember, arg[0] in C++ will always be our exe
+	if (argc == 1)
+	{
+		std::cerr << "Error: No input file/s\nUsage: PyPlus <FILE_PATHS> <args>";
+	}
+}
